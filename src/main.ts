0eff0bbd57b451041a45ecc03a342186428295cc
Vanneth <tholvanneth@gmail.com>
2024-06-23 19:07:09 +0700

    update(dependencies): added pinia store and element-plus icon

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,18 @@ import { createApp } from 'vue'
 import "@/assets/tailwind.css";
 import "@/assets/style/main.css";
 import "element-plus/theme-chalk/index.css";
+import * as ElementPlusIcon from "@element-plus/icons-vue"
+import { createPinia } from 'pinia'
 
 // @ts-ignore
 import App from '~/App.vue'
 import router from '~/router'
 
-const app = createApp(App).use(router);
+const app = createApp(App)
+	.use(router)
+	.use(createPinia());
 
+for (const [key, component] of Object.entries(ElementPlusIcon)) {
+	app.component(key, component)
+}
 router.isReady().then(() => app.mount('#app'))
