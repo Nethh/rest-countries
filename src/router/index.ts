6de4cc590f58a8836c3ef65cbdcfc85394de66a6
Vanneth <tholvanneth@gmail.com>
2024-06-23 13:54:33 +0700

    fix(vue-router): fix unable to mount the component
    Turn out i was missing the .vue binding extention that cause this unexpected behavior. by simple change from this
    ```(`@/pages/${path}`)``` to ```(`@/pages/${path}.vue`)```

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,13 @@
 import {createRouter, createWebHistory, Router, RouteRecordRaw} from 'vue-router';
 
 // simplify import path
-const page: any = (path: string) => () => import(`@/pages/${path}`).then(m => m.default || m)
+const page: any = (path: string) => () => import(`@/pages/${path}.vue`).then(m => m.default || m);
 
 const routes: Array<RouteRecordRaw> = [
 	{
 		name: 'countries',
 		path: '/',
-		component: page('countries.vue')
+		component: page('countries')
 	}
 ]
 
